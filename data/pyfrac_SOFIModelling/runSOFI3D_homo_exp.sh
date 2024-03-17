b4a0e3cf820992e1f700d6b222332866bbbaef52
cebirnie92 <cebirnie@gmail.com>
2024-03-17 14:47:33 +0300

    Detangling from hydrafrac codes - stage2

---
+++ data/pyfrac_SOFIModelling/runSOFI3D_homo_exp.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+mkdir -p outputs
+mkdir -p outputs/log
+mkdir -p outputs/snap
+mkdir -p outputs/su
+
+sofipath=~/Documents/Projects/MicroseismicModelling/SOFI3D-master/bin/sofi3D
+mpirun -np 8 ${sofipath} homo_exp.json > sofi3D.jout
+
+# Do the snap merge thing
+snapmergepath=~/Documents/Projects/MicroseismicModelling/SOFI3D-master/bin/snapmerge
+${snapmergepath} homo_exp.json
+
+# Clean up the snap files (for memory purposes)
+rm -rf ./outputs/snap/**.0*
+rm -rf ./outputs/snap/**.1*

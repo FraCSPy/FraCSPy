a9ef99d909c02b969cd469e47b848d790e5780f0
cebirnie92 <cebirnie@gmail.com>
2024-07-15 14:55:37 +0300

    fixing init file for the version file

--- fracspy/__init__.py
+++ fracspy/__init__.py
@@ -4,3 +4,13 @@ from . import mtsolvers
 from . import processing
 from . import utils
 from . import visualisation
+ 
+try:
+    from .version import version as __version__
+except ImportError:
+    # If it was not installed, then we don't know the version. We could throw a
+    # warning here, but this case *should* be rare. pylops should be installed
+    # properly!
+    from datetime import datetime
+
+    __version__ = "unknown-" + datetime.today().strftime("%Y%m%d")
